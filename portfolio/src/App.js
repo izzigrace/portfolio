3ef4859cff7db5ccd25c81625a88bffa51d6332e
Isabelle <izzigrace6@gmail.com>
2022-10-18 16:39:57 -0700

    make app

---
+++ portfolio/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="helloIAm">
+        Hello! I am:
+      </div>
+      <div className="isabelleSmith">
+        Isabelle Smith
+      </div>
+      <div className="aFullStackSE">
+        A Full-Stack Software Engineer
+      </div>
+    </div>
+  );
+}
+
+export default App;
