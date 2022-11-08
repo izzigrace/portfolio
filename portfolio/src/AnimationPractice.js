a2c983cf81938f48fdd4ea2bd771e4b2fefda4aa
Isabelle <izzigrace6@gmail.com>
2022-11-08 00:04:38 -0800

    typing animation

---
+++ portfolio/src/AnimationPractice.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './AnimationPractice.css';
+
+function AnimationPractice() {
+  return (
+    <div className="AnimationPractice">
+      {/* <svg viewBox="0 0 800 500" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" id="blobSvg">
+        <g transform="translate(177.95868110656738, 21.191181182861328)">
+          <defs>
+            <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+              <stop offset="0%" className="stopOffset1"></stop>
+              <stop offset="100%" className="stopOffset2"></stop>
+            </linearGradient>
+          </defs>
+          <path class="blob" d="M410.5,286Q399,322,402.5,383.5Q406,445,344,418Q282,391,245.5,411Q209,431,176.5,407Q144,383,105.5,360.5Q67,338,39.5,294Q12,250,31.5,202Q51,154,78.5,111.5Q106,69,151,41Q196,13,238,66.5Q280,120,306.5,132.5Q333,145,376,157Q419,169,420.5,209.5Q422,250,410.5,286Z" fill="url(#gradient)"></path>
+        </g>
+      </svg> */}
+    </div>
+  );
+}
+
+export default AnimationPractice;
--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,19 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import AnimationPractice from './AnimationPractice';
+import Typical from "react-typical";
+import Typed from 'react-typed';
 
 function App() {
+
   return (
     <div className="App">
-      <div className="helloIAm">
-        Hello! I am:
+      <div className="helloWorldIAm">
+        Hello World! I am:
       </div>
       <div className="isabelleSmith">
         Isabelle Smith
       </div>
       <div className="aFullStackSE">
-        A Full-Stack Software Engineer
+        {/* <Typical
+          steps={['A Front End Developer', 1000, 'A Back End Developer', 1000, 'A Full Stack Software Engineer.', 5000]}
+          loop={Infinity}
+          wrapper="p"
+        /> */}
+        <Typed
+          strings={[
+              'A Front End Developer',
+              'A Back End Developer',
+              'A Full Stack Software Engineer.^5000']}
+              typeSpeed={40}
+              backSpeed={50}
+              loop >
+        </Typed>
+        </div>
+      {/* <AnimationPractice /> */}
       </div>
-    </div>
   );
 }
 
