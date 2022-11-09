389a9777badece2c9b8061dd95e33d5eeccebbe9
Isabelle <izzigrace6@gmail.com>
2022-11-08 19:11:32 -0800

    complete introduction page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,37 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import AnimationPractice from './AnimationPractice';
 import Typical from "react-typical";
 import Typed from 'react-typed';
+import { useState, useEffect } from 'react';
+import Introduction from './Introduction.js';
 
 function App() {
-
   return (
     <div className="App">
-      <div className="helloWorldIAm">
-        Hello World! I am:
-      </div>
-      <div className="isabelleSmith">
-        Isabelle Smith
-      </div>
-      <div className="aFullStackSE">
-        {/* <Typical
-          steps={['A Front End Developer', 1000, 'A Back End Developer', 1000, 'A Full Stack Software Engineer.', 5000]}
-          loop={Infinity}
-          wrapper="p"
-        /> */}
-        <Typed
-          strings={[
-              'A Front End Developer',
-              'A Back End Developer',
-              'A Full Stack Software Engineer.^5000']}
-              typeSpeed={40}
-              backSpeed={50}
-              loop >
-        </Typed>
-        </div>
-      {/* <AnimationPractice /> */}
-      </div>
+      <Introduction/>
+    </div>
   );
 }
 
---
+++ portfolio/src/Introduction.js
@@ -0,0 +1,36 @@
+import Typical from "react-typical";
+import Typed from 'react-typed';
+import { useState, useEffect } from 'react';
+import React from "react";
+import './Introduction.css'
+
+function Introduction() {
+
+  const [nameDiv, setNameDiv] = useState(<div className="emptyDiv">Hi</div>);
+  setTimeout(() => {
+    setNameDiv(<div className="isabelleSmith">
+    Isabelle Smith</div>);
+  }, 2000);
+
+  return (
+    <div className="App">
+      <div className="helloWorldIAm">
+        Hello World! I am:
+      </div>
+      {nameDiv}
+      <div className="aFullStackSE">
+        <Typed
+          strings={[
+              '^5000A Front End Developer',
+              'A Back End Developer',
+              'A Full Stack Software Engineer.^5000']}
+              typeSpeed={40}
+              backSpeed={50}
+              loop >
+        </Typed>
+        </div>
+      </div>
+  );
+}
+
+export default Introduction;
